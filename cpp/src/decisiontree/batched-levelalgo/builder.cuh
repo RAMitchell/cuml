937a9761dd9f61abbe8d6e98ea92a0f9f9d0f80e
Rory Mitchell <r.a.mitchell.nz@gmail.com>
2026-06-12 11:38:37 +0200

    Fuse RF segmented scan partition write

--- cpp/src/decisiontree/batched-levelalgo/builder.cuh
+++ cpp/src/decisiontree/batched-levelalgo/builder.cuh
@@ -185,8 +185,6 @@ struct Builder {
   IdxT* colids;
   /** temporary row IDs for row-wise out-of-place partitioning */
   IdxT* partition_row_ids;
-  /** per-logical-row left partition ranks from a segmented scan */
-  IdxT* partition_left_offsets;
   /** rmm device workspace buffer */
   rmm::device_uvector<char> d_buff;
   /** pinned host buffer to store the trained nodes */
@@ -281,7 +279,6 @@ struct Builder {
     d_wsize += calculateAlignedBytes(sizeof(IdxT) * max_batch * dataset.n_sampled_cols);  // colids
     d_wsize +=
       calculateAlignedBytes(sizeof(IdxT) * dataset.n_sampled_rows);  // partition row IDs
-    d_wsize += calculateAlignedBytes(sizeof(IdxT) * max_blocks_dimx * TPB_DEFAULT);  // left ranks
 
     // all nodes in the tree
     h_wsize +=  // h_workload_info
@@ -325,8 +322,6 @@ struct Builder {
     d_wspace += calculateAlignedBytes(sizeof(IdxT) * max_batch * dataset.n_sampled_cols);
     partition_row_ids = reinterpret_cast<IdxT*>(d_wspace);
     d_wspace += calculateAlignedBytes(sizeof(IdxT) * dataset.n_sampled_rows);
-    partition_left_offsets = reinterpret_cast<IdxT*>(d_wspace);
-    d_wspace += calculateAlignedBytes(sizeof(IdxT) * max_blocks_dimx * TPB_DEFAULT);
 
     RAFT_CUDA_TRY(
       cudaMemsetAsync(done_count, 0, sizeof(int) * max_batch * n_col_blks, builder_stream));
@@ -490,7 +485,6 @@ struct Builder {
                                                             workload_info,
                                                             n_blocks_dimx,
                                                             partition_row_ids,
-                                                            partition_left_offsets,
                                                             builder_stream);
     RAFT_CUDA_TRY(cudaPeekAtLastError());
     raft::common::nvtx::pop_range();
